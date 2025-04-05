d2955adeb9ef55d6e6b3f27412fd35597a8577d2
Sunil002-git <sunilmarri886@gmail.com>
2025-04-05 18:08:54 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+
+Route::group(['account'], function() {
+    //Guest Routes
+    Route::group(['middleware' => 'guest'], function(){
+        Route::get('/account/register', [AccountController::class, 'registration'])->name('account.registration');
+        Route::post('/account/process-register', [AccountController::class, 'processRegistration'])->name('account.processRegistration');
+        Route::get('/account/login', [AccountController::class, 'login'])->name('account.login');
+        Route::post('/account/authenticate', [AccountController::class, 'authenticate'])->name('account.authenticate');
+    });
+    //Authenticate Routes
+    Route::group(['middleware' => 'auth'], function(){
+        Route::get('/account/profile', [AccountController::class, 'profile'])->name('account.profile');
+        Route::get('/account/logout', [AccountController::class, 'logout'])->name('account.logout');
+    });
+});
